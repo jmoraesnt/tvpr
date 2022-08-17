dc38f9d9e1c30420436f874ac100a0ffb8830fa2
jmoraesnt <prjmoraws@gmail.com>
2022-08-16 21:01:28 -0300

    Add bootstrap

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,20 +1,39 @@
 @extends('layouts.app')
 @section('title', 'Index')
 @section('content')
-    <h1>Listagem de Usuários (<a href="{{ route('users.create') }}">+</a>)</h1>
-    <form action="{{ route('users.index') }}" method="GET">
-        <input type="text" name="search" placeholder="Pesquisar">
-        <button>Pesquisar</button>
-    </form>
-    <ul>
-        @foreach ($users as $user)
-            <li>
-                {{ $user->name }} -
-                {{ $user->email }}
-                | <a href="{{ route('users.show', $user->id) }}">Detalhes</a>
-                | <a href="{{ route('users.edit', $user->id) }}">Editar</a>
+    <div class="container px-md-5">
 
-            </li>
-        @endforeach
-    </ul>
+        <h1>Listagem de Usuários <a class="btn btn-success" href="{{ route('users.create') }}" role="button">New</a></h1>
+        <form action="{{ route('users.index') }}" method="GET">
+            <div class="input-group mb-3">
+                <input name="search" type="text" class="form-control" placeholder="Digite sua pesquisa..."
+                    aria-label="Recipient's username" aria-describedby="button-addon2">
+                <button class="btn btn-outline-secondary" type="subimit" id="button-addon2">Pesquisar</button>
+            </div>
+        </form>
+
+        <table class="table table-hover border bg-light">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Detalhes</th>
+                    <th scope="col">Editar</th>
+                </tr>
+            </thead>
+            @foreach ($users as $user)
+                <tbody>
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td><a href="{{ route('users.show', $user->id) }}">Detalhes</a></td>
+                        <td><a href="{{ route('users.edit', $user->id) }}">Editar</a></td>
+                    </tr>
+                </tbody>
+            @endforeach
+        </table>
+    </div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
